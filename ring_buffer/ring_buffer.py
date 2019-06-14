5d7913c73cf50a5bffc0c9f7afe0edfb0e64073b
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-14 11:15:42 -0400

    ring_buffer.py complete. All tests in test_ring_buff.py pass.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,16 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        self.storage.insert(self.current, item)
+        self.storage.pop(self.current + 1)
+        if self.current == self.capacity - 1:
+            self.current = 0
+        else:
+            self.current += 1
 
-  def get(self):
-    pass
+    def get(self):
+        return [i for i in self.storage if i != None]
