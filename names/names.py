4791d39f3d541a2e1d9c3ed56c686a6d0e2de5c4
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-06-14 12:30:08 -0400

    Optimized names.py to O(n log n) using BST.

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 
+from binary_search_tree import BinarySearchTree
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -7,16 +9,21 @@ names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+# names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = BinarySearchTree()
+for row in f:
+    names_2.insert(str.strip(row))
 f.close()
 
 duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if names_2.contains(name_1):
+        duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
